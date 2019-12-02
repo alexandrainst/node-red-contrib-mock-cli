bd8feb6ddc1f22e02bde21ef9ce5458e7ddc38a6
Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
2019-12-02 14:13:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# node-red-contrib-mock-cli
+
+This is a [Node.js](https://nodejs.org) module to allow running [Node-RED](https://nodered.org) nodes from command-line, and pipe one to another, using a similar flow than what would be done in the Node-RED graphical interface.
+
+Made in December 2019 by [Alexandre Alapetite](https://alexandra.dk/alexandre.alapetite) at the [Alexandra Institute](https://alexandra.dk) for the [SynchroniCity European project](https://synchronicity-iot.eu).
+
+License: MIT
+
+## Usage
+
+Add an `index.js` file to your Node-RED node, next to a `package.json` that has a structure like `{ "node-red": {"node-name": "node-name.js"} }`:
+
+```js
+const RED = require('node-red-contrib-mock-cli');
+const noderedNode = RED.load(require.main);
+if (noderedNode) {
+	RED.run();
+} else {
+	console.error('Error loading Node-RED node!');
+}
+```
+
+The last loaded node can be accessed from `RED.node` and the full list of loaded nodes from `RED.nodes`.
+
+One can then invoke the node from command-line, passing the node name first, and then some optional node properties in JSON format:
+
+```sh
+node ./index.js node-name --firstProperty='{"Some":"JSON"}' --secondProperty='"Some text"' --thirdProperty='123'
+```
+
+The command expects JSON messages with a Node-RED structure `{"payload":"Example"}` from standard input, one line per message.
+
+The command outputs JSON messages with a Node-RED structure to standard output, one line per message.
---
+++ index.js
@@ -0,0 +1,160 @@
+/* jshint esversion:8, node:true, strict:true */
+"use strict";
+/**
+ * Node-RED mocker.
+ * 
+ * This script mocks Node-RED so that it is possible
+ * to natively call a module originally designed for Node-RED from command line without requiring Node-RED.
+ *
+ * The first command-line argument parameter must be a Node-RED module name as declared in the local `package.json['node-red']['nodes']`.
+ * The following command-line arguments must use the format `--someArgument='Hello World'` and are passed to the module via the Node-RED configuration.
+ *
+ * @author Alexandre Alapetite <https://alexandra.dk/alexandre.alapetite>
+ * @copyright Alexandra Institute <https://alexandra.dk> for the SynchroniCity European project <https://synchronicity-iot.eu> as a contribution to FIWARE <https://www.fiware.org>
+ * @license MIT
+ * @version 1.0
+ * @date 2019-11-28 / 2019-12-02
+ */
+
+const EventEmitter = require('events').EventEmitter;
+
+function Context() {
+	this._values = {};
+	this.get = key => this._values[key];
+	this.set = (key, value) => this._values[key] = value;
+}
+
+const RED = {
+		node: null,
+		nodes: {
+			config: {},
+			createNode: (node, config) => {
+				node.eventEmitter = new EventEmitter();
+				node.on = (eventName, listener) => node.eventEmitter.on(eventName, listener);
+				node.debug = msg => {
+					node.eventEmitter.emit('debug', msg);
+					if (node.eventEmitter.listenerCount('debug') <= 0) {
+						console.warn(msg);
+					}
+				};
+				node.error = msg => {
+					node.eventEmitter.emit('error', msg);
+					if (node.eventEmitter.listenerCount('error') <= 0) {
+						console.warn(msg);
+					}
+				};
+				node.input = msg => node.eventEmitter.emit('input', msg);
+				node.log = msg => {
+					node.eventEmitter.emit('log', msg);
+					if (node.eventEmitter.listenerCount('log') <= 0) {
+						console.warn(msg);
+					}
+				};
+				node.send = msg => node.eventEmitter.emit('send', msg);
+
+				node.status = status => {};
+
+				const context = new Context();
+				context.flow = new Context();
+				context.global = new Context();
+				node.context = () => context;
+
+				RED.node = node;
+				RED.nodes.list.push(node);
+			},
+			list: [],
+			registerType: (name, f) => new f(RED.nodes.config),
+		},
+		load: (main = null) => {
+			if (!main) {
+				main = require.main;
+			}
+			try {
+				const args = process.argv.slice(2);
+				const config = main.require('./package.json');
+				const module = args && args.length > 0 ? args[0] : '';
+				const script = module && config && config['node-red'] && config['node-red'].nodes ? config['node-red'].nodes[module] : '';
+				if (script) {
+					//Pass all command-line arguments to the future Node-RED module
+					RED.nodes.config = {};
+					for (let param of args.slice(1)) {
+						const match = /^--([a-zA-Z0-9_]+)=(.+)?$/g.exec(param);
+						if (match) {
+							const paramKey = match[1];
+							const paramValue = JSON.parse(match[2]);
+							RED.nodes.config[paramKey] = paramValue;
+						} else {
+							console.warn('Invalid parameter for Node-RED module: ' + param);
+						}
+					}
+
+					const module = main.require('./' + script);
+					module(RED);
+					return RED.node;
+				}
+			} catch (ex) {
+				console.error(ex);
+			}
+			console.error('Invalid argument for Node-RED module! Check package.json');
+			console.error('Usage: node ./index.js node-name --firstProperty="Hello World"');
+			return false;
+		},
+		run: () => {
+			//Number of STDIN lines for which we have not received a result yet
+			let nbAwaited = 0;
+			let done = false;
+
+			//When our Node-RED module sends/outpus a new message
+			RED.node.on('send', msg => {
+				console.log(JSON.stringify(msg));
+				nbAwaited--;
+				if (done && nbAwaited <= 0) {
+					process.exit(0);
+				}
+			});
+
+			RED.node.on('error', msg => {
+				console.error(msg);
+			});
+
+			RED.node.on('log', msg => {
+				console.warn(msg);
+			});
+
+
+			const readline = require('readline');
+
+			const rl = readline.createInterface({
+				input: process.stdin,
+				terminal: false,
+			});
+
+			//Read JSON messages from standard input
+			rl.on('line', line => {
+				try {
+					const msg = JSON.parse(line);
+					nbAwaited++;
+					RED.node.input(msg);
+				} catch (ex) {
+					console.error('Invalid JSON input: ' + ex);
+				}
+			});
+
+			rl.on('error', () => {
+				console.error('==== STDIN error ====');
+				done = true;
+			});
+
+			rl.on('pause', () => {
+				console.error('==== STDIN paused ====');
+				done = true;
+			});
+
+			rl.on('close', () => {
+				console.error('==== STDIN closed ====');
+				done = true;
+			});
+		},
+	};
+
+module.exports = RED;
