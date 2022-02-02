f39d108041967b71795ebc1505f1be50cd740fcf
Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
2022-02-02 22:36:17 +0100

    Add RED.util.cloneMessage

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
  * @copyright Alexandra Institute <https://alexandra.dk> for the SynchroniCity European project <https://synchronicity-iot.eu>
  * as a contribution to FIWARE <https://www.fiware.org>.
  * @license MIT
- * @date 2019-11-28 / 2022-01-24
+ * @date 2019-11-28 / 2022-02-02
  */
 
 const EventEmitter = require('events').EventEmitter;
@@ -195,6 +195,10 @@ const RED = {
 			console.warn('==== STDIN closed ====');
 			done = true;
 		});
+	},
+
+	util: {
+		cloneMessage: (msg) => JSON.parse(JSON.stringify(msg)),
 	},
 };
 
